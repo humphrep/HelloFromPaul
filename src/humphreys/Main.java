79806e03441008621a3d8eb4c5b89b8b63ca8056
Paul Humphreys <humphrep@yahoo.com>
2021-01-25 18:37:19 -0600

    Initial commit

---
+++ src/humphreys/Main.java
@@ -0,0 +1,11 @@
+package humphreys;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Paul";
+        System.out.printf("Hello, my name is %s%n", name);
+
+    }
+}
