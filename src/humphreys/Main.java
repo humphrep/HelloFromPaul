8046a47440510960ec256d7376592ce31f83a07c
meflav <tru19006@byui.edu>
2021-01-26 20:40:39 -0500

    Add my name...

--- src/humphreys/Main.java
+++ src/humphreys/Main.java
@@ -10,5 +10,7 @@ public class Main {
         String name2 = "Clark";
         System.out.printf("Hello, my name is %s%n", name2);
 
+        String myname3 = "Flavio";
+        System.out.println("Hello, My name is " + myname3);
     }
 }
