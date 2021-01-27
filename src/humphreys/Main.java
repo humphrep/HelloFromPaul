94e29a5e5a390076f7bd5b91dac940cfe78b051a
Clark Mulvey <clarkgmulvey2@gmail.com>
2021-01-26 18:46:56 -0700

    Clark updated message

--- src/humphreys/Main.java
+++ src/humphreys/Main.java
@@ -7,8 +7,8 @@ public class Main {
         String paulName = "Paul";
         System.out.printf("Hello, my name is %s%n", paulName);
 
-        String name2 = "Clark";
-        System.out.printf("Hello, my name is %s%n", name2);
+        String clarkName = "Clark";
+        System.out.printf("Hello, %s, my name is %s%n", paulName, clarkName);
 
         String myname3 = "Flavio";
         System.out.println("Hello, My name is " + myname3);
